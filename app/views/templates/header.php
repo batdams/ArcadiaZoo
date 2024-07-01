344bc128bab986b9d85fd087a9537cd30256c820
batdams <damienbilliau@gmail.com>
2024-07-01 14:44:17 +0200

    redef routesAjax10

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arcadia Web Application</title>
     <link rel="stylesheet" href="/public/styles/styles.css">
-    <link rel="icon" href="<?php echo BASE_URL;?>/public/pictures.flavicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo BASE_URL;?>/public/pictures/flavicon.ico" type="image/x-icon">
 </head>
 <body>
     <header>
